40b00a350778fda2ad676e4584bc8a10dfbed22f
Jesus Santiago Manzano <jesussantiagomanzano@gmail.com>
2024-04-17 19:23:04 -0600

    Uploading spring boot files

---
+++ src/main/java/com/myapp/unsij/WsBanckendProjectApplication.java
@@ -0,0 +1,13 @@
+package com.myapp.unsij;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsBanckendProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WsBanckendProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myapp/unsij/WsBanckendProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myapp.unsij;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WsBanckendProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
