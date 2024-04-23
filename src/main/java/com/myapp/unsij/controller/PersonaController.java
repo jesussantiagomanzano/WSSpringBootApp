3a056018b785b50b93e8a2597468582ccf055c14
Jesus Santiago Manzano <jesussantiagomanzano@gmail.com>
2024-04-22 19:22:56 -0600

    Configuring database properties

--- src/main/java/com/myapp/unsij/controller/PersonaController.java
+++ src/main/java/com/myapp/unsij/controller/PersonaController.java
@@ -1,17 +1,30 @@
 package com.myapp.unsij.controller;
 
 import com.myapp.unsij.entity.Persona;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
+@RequestMapping(value = "personas")
 public class PersonaController {
 
-    @GetMapping("/persona")
-    public Persona personas(){
+    List<Persona> personas  = new ArrayList<>();
 
-        return new Persona("Jesus", "Santiago", "SAMJ");
+    @GetMapping()
+    public List<Persona> personas(){
+
+        return personas;
     }
+
+    @PostMapping
+    public Persona addPersona(@RequestBody Persona persona){
+        personas.add(persona);
+       return persona;
+    }
+
+
 
 
 }
--- src/main/java/com/myapp/unsij/entity/Persona.java
+++ src/main/java/com/myapp/unsij/entity/Persona.java
@@ -1,14 +1,26 @@
 package com.myapp.unsij.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Setter
 @Getter
-@AllArgsConstructor
 @NoArgsConstructor
+@AllArgsConstructor
+@Table
+@Entity
 public class Persona {
+
+    @Id
+    private Long id;
     private String name;
     private String lastName;
     private String curp;
+    private Integer age;
+    private String email;
+
+
 
 }
